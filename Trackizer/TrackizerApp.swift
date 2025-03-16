007cb8d6d042f2ea25837af9436b0e889185f151
André <andrenestor0503@gmail.com>
2025-03-16 18:46:57 -0300

    Initial Commit

---
+++ Trackizer/TrackizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackizerApp.swift
+//  Trackizer
+//
+//  Created by André Nestor on 16/03/25.
+//
+
+import SwiftUI
+
+@main
+struct TrackizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
